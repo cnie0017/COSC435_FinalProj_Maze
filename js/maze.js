b92b884d2b0753964c693e900e4e29d52aa50baa
cn0017 <cnie@colgate.edu>
2019-12-12 14:34:22 -0500

    minor change in maze

--- js/maze.js
+++ js/maze.js
@@ -1,66 +1,45 @@
-function generateMaze(dimension) {
+function generateMaze(size) {
 
-    function iterate(field, x, y) {
-        field[x][y] = false;
+    function iterate(maze, x, y) {
+        maze[x][y] = false;
         while(true) {
             directions = [];
-            if(x > 1 && field[x-2][y] == true) {
+            if(x > 1 && maze[x-2][y] == true) {
                 directions.push([-1, 0]);
             }
-            if(x < field.dimension - 2 && field[x+2][y] == true) {
+            if(x < maze.size - 2 && maze[x+2][y] == true) {
                 directions.push([1, 0]);
             }
-            if(y > 1 && field[x][y-2] == true) {
+            if(y > 1 && maze[x][y-2] == true) {
                 directions.push([0, -1]);
             }
-            if(y < field.dimension - 2 && field[x][y+2] == true) {
+            if(y < maze.size - 2 && maze[x][y+2] == true) {
                 directions.push([0, 1]);
             }
             if(directions.length == 0) {
-                return field;
+                return maze;
             }
             dir = directions[Math.floor(Math.random()*directions.length)];
-            field[x+dir[0]][y+dir[1]] = false;
-            field = iterate(field, x+dir[0]*2, y+dir[1]*2);
+            maze[x+dir[0]][y+dir[1]] = false;
+            maze = iterate(maze, x+dir[0]*2, y+dir[1]*2);
         }
     }
 
-    // Initialize the field.
-    var field = new Array(dimension);
-    field.dimension = dimension;
-    for(var i = 0; i < dimension; i++) {
-        field[i] = new Array(dimension);
-        for (var j = 0; j < dimension; j++) {
-            field[i][j] = true;
+    // Initialize the maze.
+    var maze = new Array(size);
+    maze.size = size;
+    for(var i = 0; i < size; i++) {
+        maze[i] = new Array(size);
+        for (var j = 0; j < size; j++) {
+            maze[i][j] = true;
         }
     }
 
     // Gnerate the maze recursively.
-    field = iterate(field, 1, 1);
+    maze = iterate(maze, 1, 1);
 
-    return field;
+    return maze;
 
 }
 
-function CreateMazeMesh(maze) {
-    console.log("size is",size);
-    //to be changed later
-      for (var i = 0; i < maze.dimension; i++) {
-          for (var j = 0; j < maze.dimension; j++) {
-              var mazeObj = maze[i][j];
-              if (mazeObj) {
-          if (i == entranceXidx && j==entranceZidx){//entrance
-            createTree(entranceX,entranceZ,blue);
-            maze[i+1][j] = true;
-          }
-          else if (i == exitXidx && j == exitZidx){//exit
-            //exit location = (-300+exitZidx*distance, -300+exitXidx*distance)
-            createTree(-300+j*distance,-300+i*distance,yellow);
-          }
-          else{
-            createTree(-300+j*distance,-300+i*distance,green);
-          }
-        }
-      }
-     }
-  }
+
